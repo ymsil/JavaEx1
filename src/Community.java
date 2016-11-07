24b4b9ab7eda3db0b5a731a2baf8c0b2d5e46f34
stesh <ymsil719@gmail.com>
2016-11-07 19:02:39 +0200

    almost finished ex1. further information needed to complete last function. and tests...

--- src/Community.java
+++ src/Community.java
@@ -5,6 +5,56 @@ import java.util.ArrayList;
  */
 public class Community
 {
-    public static ArrayList<CommunityMember> communityMembers = new ArrayList<CommunityMember>();
-    //private int
+    public static ArrayList<CommunityMember> communityMembers = new ArrayList<>();
+
+    private int balance;
+    private float moneyForDonations;
+
+    public int getBalance() {
+        return balance;
+    }
+    public void setBalance(int balance)
+    {
+        this.balance = balance;
+        this.moneyForDonations = (float) (this.balance*0.2);
+    }
+    public float getMoneyForDonations()
+    {
+        moneyForDonations = (float) (balance*0.2);
+        return moneyForDonations;
+    }
+
+
+    public void addMember(CommunityMember member)
+    {
+        communityMembers.add(member);
+    }
+    public int expectedTaxFromMembers()
+    {
+        int tax = 0;
+        for (CommunityMember member : communityMembers)
+        {
+            tax += member.taxForMember();
+        }
+        return tax;
+    }
+
+    public float DonationRequest(CommunityMember member)
+    {
+        int DonationToMember = member.maxChartiyForMember();
+        if (DonationToMember < moneyForDonations)
+        {
+            int newBalance = balance - DonationToMember;
+            setBalance(newBalance);
+            return DonationToMember;
+        }
+        else
+        {
+            int newBalance = (balance - (int)(moneyForDonations));
+            setBalance(newBalance);
+            return (int)(moneyForDonations);
+        }
+    }
+
+
 }
